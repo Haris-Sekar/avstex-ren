c5eca1bb69caa0051e8789c2009c47ae182f0195
Haris Sekar <sharis14003@gmail.com>
2021-11-01 07:50:56 +0530

    db connection update

--- php/conn.php
+++ php/conn.php
@@ -17,7 +17,7 @@
 $host='b4qjlowsvqnxh584yjnm-mysql.services.clever-cloud.com:3306';
 $username='uy0caq08nnmiknzz';
 $password='jmC4ecTwJDAGnw5IsTu8';
-$database='b4qjlowsvqnxh584yjnm';
+$dbname='b4qjlowsvqnxh584yjnm';
 
 $conn=mysqli_connect($host,$username,$password,$dbname);
 if(mysqli_connect_error()){
