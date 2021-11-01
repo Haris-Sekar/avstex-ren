0c7186e893d4ea3c8f7f3e1d62755bdbeb479dbc
Haris Sekar <sharis14003@gmail.com>
2021-11-01 09:08:19 +0530

    bug fixed

--- php/admin_products.php
+++ php/admin_products.php
@@ -7,6 +7,16 @@ $product_res=mysqli_query($conn,$product_sql);
 
 
 ?>
+<div class="nav_container1">
+    <div class="nav1">
+        <ul>
+            <a href="./admin_size_master.php"><li id="nav_items1">Add size group</li></a>
+            <a href="./admin_add_products.php"><li id="nav_items1">Add products</li></a>
+            <a href="./admin_rate_select_product.php"><li id="nav_items1">Set Rates</li></a>
+        </ul>
+    </div>
+</div>
+
 
 <div class="products_display">
     <?php while($row=mysqli_fetch_array($product_res,MYSQLI_ASSOC)){?>
