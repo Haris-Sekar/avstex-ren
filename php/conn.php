14590314c11a68240b7c4bbd0e647684a84e1226
Haris Sekar <sharis14003@gmail.com>
2021-11-01 07:49:51 +0530

    db connection update

--- php/conn.php
+++ php/conn.php
@@ -7,12 +7,17 @@
 // $dbname="avstex_ren";
 
 // //infinity free db connection
-$host="sql312.epizy.com";
-$username="epiz_29179456";
-$password="BOW9m6bqRrK";
-$dbname="epiz_29179456_avstex";
+// $host="sql312.epizy.com";
+// $username="epiz_29179456";
+// $password="BOW9m6bqRrK";
+// $dbname="epiz_29179456_avstex";
 
+//clever cloud DB connect Details
 
+$host='b4qjlowsvqnxh584yjnm-mysql.services.clever-cloud.com:3306';
+$username='uy0caq08nnmiknzz';
+$password='jmC4ecTwJDAGnw5IsTu8';
+$database='b4qjlowsvqnxh584yjnm';
 
 $conn=mysqli_connect($host,$username,$password,$dbname);
 if(mysqli_connect_error()){
